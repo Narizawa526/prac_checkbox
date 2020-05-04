5b2d46980579bd987568ec144813d3741aafea97
Kino Kiaro <kiarokino@gmail.com>
2020-05-04 21:20:43 +0900

    チェックボックスをテキストに倣って作成するところまで。

--- app/controllers/c_search_controller.rb
+++ app/controllers/c_search_controller.rb
@@ -1,6 +1,15 @@
 class CSearchController < ApplicationController
 
   def search
+    if request.post? then
+      if params['check1'] then
+        @msg = 'あなたの悩みは「家族・親戚からの暴言・無視」でよろしいですか？'
+      else
+        @msg = '選択されていません。'
+      end
+    else
+      @msg = '選択してください。'
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'c_search/search'
   get 'c_search', to:'c_search#search'
+  post 'c_search', to:'c_search#search'
+  post 'c_search/search'
 end
